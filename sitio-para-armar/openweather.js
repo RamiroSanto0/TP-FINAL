f2696e19663740bab74d6d6f67acbe1e61d5b925
RamiroSanto0 <108102309+RamiroSanto0@users.noreply.github.com>
2023-01-30 19:50:33 -0300

    Add files via upload

---
+++ sitio-para-armar/openweather.js
@@ -0,0 +1,21 @@
+let clima = {
+    apiKey:"f592fe2e6702022144ffdaa1087b920f",
+    fetchClima:function(ciudad){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + ciudad + "&units=metric&appid=" + this.apiKey)
+        .then((response)=>{
+            return response.json();
+        })
+        .then((data)=>this.mostrarClima(data));
+    },
+    mostrarClima:function(data){
+        const {temp} = data.main;
+        document.querySelector("#temp").innerHTML = temp + "&#8451;"
+    }
+}
+
+let ciudad = document.querySelector("#ciudad");
+ciudad.addEventListener("change", function(){
+    clima.fetchClima(ciudad.value)
+})
+
+clima.fetchClima("Buenos Aires");
